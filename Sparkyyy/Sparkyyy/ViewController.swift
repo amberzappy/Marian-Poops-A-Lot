22126a3d559ce69d91aaa526cd60174837ef4e5c
Owner <ambermcmains@yahoo.com>
2017-10-06 14:30:33 -0700

    My first commit

--- Sparkyyy/Sparkyyy/ViewController.swift
+++ Sparkyyy/Sparkyyy/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Swift project
+//  Sparkyyy
 //
 //  Created by Owner on 9/21/17.
 //  Copyright © 2017 Owner. All rights reserved.
@@ -9,28 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var theLabel: UILabel!
+    @IBOutlet weak var Spark: UILabel!
     
-    @IBOutlet weak var theLabel2: UILabel!
+    @IBOutlet weak var Spark2: UILabel!
     
-    var tapCount = 0
-    
-    @IBAction func buttonTapped(_ sender: Any) {
+    var  tapCount = 0
+   
+    @IBAction func buttonTapped2(_ sender: Any) {
+        Spark2.text = "tI sserP t'noD esaelP"
+    }
+    @IBAction func buttonTapped1(_ sender: Any) {
         tapCount = tapCount + 1
-        if tapCount >= 10{
-        theLabel.text = "I'm all tapped out, man"
+        if tapCount >= 10 {
+            Spark.text = "I'm all tapped out, man"
         }
     }
-    @IBAction func button2Tapped(_ sender: Any) {
-        theLabel2.text = "Buttons, cooler than you!"
-    }
-    override func viewDidLoad() {
+       override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.blue
-        
-    
+               self.view.backgroundColor = UIColor.cyan
     }
 
     override func didReceiveMemoryWarning() {
